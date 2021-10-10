7d36cc08b01604843d03ca4c3c313b7c043da64f
fupeng <fupenglxy@gmail.com>
2021-10-10 16:46:54 +0800

    fix: useSafeState typing

--- src/hooks/useSafeState.ts
+++ src/hooks/useSafeState.ts
@@ -2,11 +2,15 @@ import { useState, useCallback } from 'react';
 import type { Dispatch, SetStateAction } from 'react';
 import useMountedState from './useMountedState';
 
-function useSafeState<S = undefined>(
-  initialState: S | (() => S),
-): [S | undefined, Dispatch<SetStateAction<S | undefined>>] {
+/**
+ * 同 useState 一样使用，组件卸载会阻止 setState
+ * @param initialState
+ */
+function useSafeState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
+function useSafeState<S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>];
+function useSafeState<S>(initialState?: S | (() => S)) {
   const mountedState = useMountedState();
-  const [state, setState] = useState<S>(initialState);
+  const [state, setState] = useState<S>(initialState!);
   const setCurrentState = useCallback(
     (currentState) => {
       if (!mountedState()) return;
