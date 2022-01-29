0c43a2e372fd97bc19fc386153c51e97a8c6240d
fupeng <fupenglxy@gmail.com>
2022-01-30 01:12:32 +0800

    feat: useCallbackState

--- src/hooks/useCallbackState.ts
+++ src/hooks/useCallbackState.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import type { SetStateAction } from 'react';
 
 type Dispatch<A, V> = (value: A, callback?: (value: V) => void) => void;
@@ -6,19 +6,28 @@ type Dispatch<A, V> = (value: A, callback?: (value: V) => void) => void;
 /**
  * setState callback new val
  * @param initialState
+ * @return [state, setStateWithCallback, getState]
  */
-function useCallbackState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>, S>];
+function useCallbackState<S>(
+  initialState: S | (() => S),
+): [S, Dispatch<SetStateAction<S>, S>, () => S];
 function useCallbackState<S = undefined>(): [
   S | undefined,
   Dispatch<SetStateAction<S | undefined>, S | undefined>,
+  () => S | undefined,
 ];
 function useCallbackState<S>(initialState?: S | (() => S)) {
   const [state, setState] = useState(initialState);
   const cbRef = useRef<(value: S) => void>();
 
+  const stateRef = useRef(state);
+  stateRef.current = state;
+
   useEffect(() => {
     cbRef.current?.(state!);
   }, [state]);
+
+  const getState = useCallback(() => stateRef.current, []);
 
   return [
     state,
@@ -26,6 +35,7 @@ function useCallbackState<S>(initialState?: S | (() => S)) {
       setState(value);
       cbRef.current = cb;
     },
+    getState,
   ];
 }
 
